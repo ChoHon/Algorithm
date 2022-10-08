7f7c790f6220a800240203101f632e407633a007
chohon <siaksiakx@gmail.com>
2022-10-08 17:35:01 +0900

    2637.py

---
+++ week 03/2637.py
@@ -0,0 +1,63 @@
+import sys
+from heapq import *
+
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+
+
+def assemble_toy():
+    graph = [[] for x in range(n + 1)]
+    in_degree = [0] * (n + 1)
+    for i in range(m):
+        start, end, cnt = map(int, input().split())
+        graph[start].append([end, cnt])
+        in_degree[end] += 1
+
+    rank_order = []
+
+    heap = []
+    for i in range(1, n + 1):
+        if in_degree[i] == 0:
+            heappush(heap, i)
+            rank_order.append(i)
+
+    while heap:
+        pop = heappop(heap)
+
+        for end, cnt in graph[pop]:
+            in_degree[end] -= 1
+            if in_degree[end] == 0:
+                heappush(heap, end)
+                rank_order.append(end)
+
+    anw = {x: 0 for x in range(1, n + 1)}
+    anw[n] = 1
+
+    basic = []
+    for v in rank_order:
+        if not graph[v]:
+            basic.append(v)
+
+        for comp, cnt in graph[v]:
+            anw[comp] += cnt * anw[v]
+
+    return [[x, anw[x]] for x in sorted(basic)]
+
+
+for i in assemble_toy():
+    print(i[0], i[1])
+
+"""
+7
+8
+5 1 2
+5 2 2
+7 5 2
+6 5 2
+6 3 3
+6 4 4
+7 6 3
+7 4 5
+"""
