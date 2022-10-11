3125f757ae9bd620d636559e3cc73eb43f8f56b0
chohon <siaksiakx@gmail.com>
2022-10-11 14:55:50 +0900

    연산자 끼워넣기

--- week 03/14888.py
+++ week 03/14888.py
@@ -1,43 +1,57 @@
+import sys
+
+input = sys.stdin.readline
+
 n = int(input())
-l = list(map(int, input().split()))
-oper_l = list(map(int, input().split()))
+num_arr = list(map(int, input().split()))
+oper_arr = list(map(int, input().split()))
 
-max_ = -1e9
-min_ = 1e9
+max_result = -1e9
+min_result = 1e9
 
-def divide(a, b):
-    if a < 0 and b > 0:
-        return ((a * -1) // b) * -1
+
+def insert_oper(k, oper_arr, result):
+    global max_result, min_result
+
+    if sum(oper_arr) == 0:
+        max_result = max(max_result, result)
+        min_result = min(min_result, result)
+
     else:
-        return a // b
+        if oper_arr[0] > 0:
+            oper_arr[0] -= 1
+            insert_oper(k + 1, oper_arr, result + num_arr[k + 1])
+            oper_arr[0] += 1
 
-def insert_oper(o, n, l, oper_l, result):
-    global max_, min_
+        if oper_arr[1] > 0:
+            oper_arr[1] -= 1
+            insert_oper(k + 1, oper_arr, result - num_arr[k + 1])
+            oper_arr[1] += 1
 
-    if o == n - 1:
-        max_ = max(max_, result)
-        min_ = min(min_, result)
-    
-    else:
-        if oper_l[0] > 0:
-            oper_l[0] -= 1
-            insert_oper(o + 1, n, l, oper_l, result + l[o + 1])
-            oper_l[0] += 1
-        
-        if oper_l[1] > 0:
-            oper_l[1] -= 1
-            insert_oper(o + 1, n, l, oper_l, result - l[o + 1])
-            oper_l[1] += 1
+        if oper_arr[2] > 0:
+            oper_arr[2] -= 1
+            insert_oper(k + 1, oper_arr, result * num_arr[k + 1])
+            oper_arr[2] += 1
 
-        if oper_l[2] > 0:
-            oper_l[2] -= 1
-            insert_oper(o + 1, n, l, oper_l, result * l[o + 1])
-            oper_l[2] += 1
+        if oper_arr[3] > 0:
+            oper_arr[3] -= 1
+            insert_oper(k + 1, oper_arr, int(result / num_arr[k + 1]))
+            oper_arr[3] += 1
 
-        if oper_l[3] > 0:
-            oper_l[3] -= 1
-            insert_oper(o + 1, n, l, oper_l, divide(result, l[o + 1]))
-            oper_l[3] += 1
 
-insert_oper(0, n, l, oper_l, l[0])
-print(max_, min_)
+insert_oper(0, oper_arr, num_arr[0])
+print(max_result, min_result)
+
+"""
+2
+5 6
+0 0 1 0
+
+3
+3 4 5
+1 0 1 0
+
+6
+1 2 3 4 5 6
+2 1 1 1
+"""
