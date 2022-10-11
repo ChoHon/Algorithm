29218b3410b3f2887bbf5d69288e0dbf76201090
chohon <siaksiakx@gmail.com>
2022-10-11 15:13:50 +0900

    빙산 코멘트

--- week 03/2573.py
+++ week 03/2573.py
@@ -24,32 +24,41 @@ def iceberg(arr):
                     if 0 <= nx < n and 0 <= ny < m and arr[nx][ny]:
                         linked.append((nx, ny))
 
+                # key : 빙산의 좌표
+                # value : 0 - 방산의 높이
+                #         1 - 주변 빙산의 좌표 리스트
+                #         2 - 순회시 방문 여부
                 graph[(x, y)] = [arr[x][y], linked, False]
 
+    # 빙산이 다 없어질 때까지
     while graph:
         for ice in graph.keys():
             graph[ice][2] = False
 
+        # DFS로 빙산들 순회
         que = [list(graph.keys())[0]]
-
         while que:
-            pop = que.pop()
+            cur_ice = que.pop()
 
-            if graph[pop][2]:
+            if graph[cur_ice][2]:
                 continue
-            graph[pop][2] = True
+            graph[cur_ice][2] = True
 
-            for ice in graph[pop][1]:
+            for ice in graph[cur_ice][1]:
                 que.append(ice)
 
+        # 방문하지 않은 빙산이 있다면(=빙산이 분리되었다면)
+        # 걸린 시간을 리턴
         for ice in graph.keys():
             if not graph[ice][2]:
                 return cnt
 
         cnt += 1
+        # 주변의 빈칸만큼 빙산 높이 감소
         for ice in graph.keys():
             graph[ice][0] -= 4 - len(graph[ice][1])
 
+        # 빙산 높이가 없어지면 graph에서 삭제
         for ice in list(graph.keys()):
             if graph[ice][0] <= 0:
                 for near in graph[ice][1]:
