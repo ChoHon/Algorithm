ad1289245ef77ced6dc03b8ec2c485caf1cd3ba1
chohon <siaksiakx@gmail.com>
2022-10-11 15:32:15 +0900

    미로 탐색

--- week 03/2178.py
+++ week 03/2178.py
@@ -8,21 +8,23 @@ arr = [list(input().strip()) for x in range(n)]
 
 
 def maze(arr):
-    finish = [n - 1, m - 1]
-    que = deque([[0, 0]])
-    arr[0][0] = 1
-    direction = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
 
+    finish = [n - 1, m - 1]
+    arr[0][0] = 1
+
+    que = deque([[0, 0]])
     while que:
         x, y = que.popleft()
 
         for i in range(4):
-            dx = x + direction[i][0]
-            dy = y + direction[i][1]
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-            if 0 <= dx < n and 0 <= dy < m and arr[dx][dy] == "1":
-                que.append([dx, dy])
-                arr[dx][dy] = arr[x][y] + 1
+            if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] == "1":
+                que.append([nx, ny])
+                arr[nx][ny] = arr[x][y] + 1
 
     return arr[n - 1][m - 1]
 
