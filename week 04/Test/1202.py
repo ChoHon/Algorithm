7074c60574ff923fde3f46171e2fce68742363fd
chohon <siaksiakx@gmail.com>
2022-10-20 19:39:44 +0900

    보석 도둑 수정

--- week 04/Test/1202.py
+++ week 04/Test/1202.py
@@ -1,9 +1,5 @@
-# 시간초과
-# 틀림
-
 import sys
 from heapq import *
-from bisect import *
 
 
 input = sys.stdin.readline
@@ -14,23 +10,42 @@ b_arr = [int(input()) for x in range(k)]
 
 
 def jewel_thief(j_arr, b_arr):
-    j_arr = [(-y, x) for x, y in j_arr]
-    b_arr.sort()
     heapify(j_arr)
+    b_arr.sort()
 
     result = 0
-    while b_arr and j_arr:
-        value, jewel_w = heappop(j_arr)
-        value = -value
+    temp_arr = []
+    for bag_w in b_arr:
+        while j_arr and j_arr[0][0] <= bag_w:
+            weight, value = heappop(j_arr)
+            heappush(temp_arr, -value)
 
-        idx = bisect_left(b_arr, jewel_w)
-        if idx > len(b_arr) - 1:
-            continue
-
-        result += value
-        del b_arr[idx]
+        if temp_arr:
+            result -= heappop(temp_arr)
 
     return result
 
 
 print(jewel_thief(j_arr, b_arr))
+
+"""
+4 2
+4 100
+5 110
+6 90
+7 80
+5
+7
+
+4 4
+1 100
+2 200
+13 300
+10 500
+10
+10
+10
+14
+
+1100
+"""
